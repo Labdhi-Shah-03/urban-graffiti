bec8bfe5bcb3348b8eef399e292e93cf10a48616
Labdhi-Shah-03 <labdhi.techcronus@gmail.com>
2024-05-01 18:49:48 +0530

    modifed signup page code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,10 +2,10 @@ function togglePasswordVisibility(icon) {
   var inputField = icon.previousElementSibling;
   if (inputField.type === "password") {
       inputField.type = "text";
-      icon.innerHTML = '<img src="assets/image/pass-hide.svg" alt="Show Password">';
+      icon.innerHTML = '<img src="./assets/images/pass-hide.svg" alt="Show Password">';
      
   } else {
       inputField.type = "password";
-      icon.innerHTML = '<img src="assets/image/eye-slash.svg" alt="hidePassword">';
+      icon.innerHTML = '<img src="./assets/images/eye-slash.svg" alt="hidePassword">';
   }
 }
